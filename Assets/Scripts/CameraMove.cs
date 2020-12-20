0afd7b192c350c4d4d208c5546f5c55bfd03beb3
xzxADIxzx <valerazhuk20122006@gmail.com>
2020-12-20 15:07:18 +0300

    Добавил плавную камеру и перекинул скрипты в Sripts

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [Header("Objects")]
+    public GameObject player;
+
+    float x;
+    float y;
+    float xVel;
+    float yVel;
+    
+    void Update()
+    {
+        x = Mathf.SmoothDamp(x, player.transform.position.x, ref xVel, 0.5f);
+        y = Mathf.SmoothDamp(y, player.transform.position.y, ref yVel, 0.5f);
+        transform.position = new Vector3(x, y, -10f); 
+    }
+}
