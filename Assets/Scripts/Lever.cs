35d180c9997043a9a0bd8eb16f4abf6ee8342453
xzxADIxzx <valerazhuk@gmail.com>
2020-12-21 00:31:39 +0300

    ...

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [Header("Values")]
+    public bool isActive;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isActive)
+                anim.SetTrigger("Disable");
+            else
+                anim.SetTrigger("Enable");
+            isActive = !isActive;
+        }
+    }
+}
