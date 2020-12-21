b2b5c988934ca2b179eb7b40a75af16d2d653b4e
xzxADIxzx <valerazhuk@gmail.com>
2020-12-21 16:50:11 +0300

    ...

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
+    [Header("Objects")]
+    public GameObject objectToBeActived;
+    public GameObject selected;
     [Header("Values")]
     public bool isActive;
     Animator anim;
 
+    public void Active()
+    {
+        isActive = !isActive;
+        if (isActive)
+            anim.SetTrigger("Enable");
+        else
+            anim.SetTrigger("Disable");
+        //objectToBeActived.GetComponent<Active>().Active(isActive);
+    }
+
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
-    }
-    
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (isActive)
-                anim.SetTrigger("Disable");
-            else
-                anim.SetTrigger("Enable");
-            isActive = !isActive;
-        }
     }
 }
---
+++ Assets/Scripts/SettingsControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SettingsControl
+{
+    public static KeyCode up = KeyCode.W;
+    public static KeyCode down = KeyCode.S;
+    public static KeyCode left = KeyCode.A;
+    public static KeyCode right = KeyCode.D;
+    public static KeyCode jump = KeyCode.Space;
+    public static KeyCode active = KeyCode.E;
+}
