8636d7da838a66dda17954048314861997e42b69
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-01-13 22:06:03 +0300

    ...

---
+++ Assets/Animations/CutScene/CutScene1/Download.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Download : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+    	SceneManager.LoadScene("LVL1");
+    }
+}
